6d915bac109f70582936abd7dee5056b3d81266a
Roman Anurov <anurovroman@yandex.ru>
2023-10-04 01:15:04 +0300

    fix: mob

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -4,32 +4,60 @@ import "./HeaderNav.scss";
 import "../Button/Button";
 import Button from "../Button/Button";
 import phoneIcon from "../../images/PhoneIconSvg.svg";
-import favoritesIcon from "../../images/FavouritesIconSvg.svg";
+import phoneIconMob from "../../images/PhoneIconMob.svg";
+import favouritesIcon from "../../images/FavouritesIconSvg.svg";
+import favouritesIconMob from "../../images/FavouritesIconMob.svg";
 import profileIcon from "../../images/ProfileIconSvg.svg";
 import basketIconWhite from "../../images/BasketIconWhiteSvg.svg";
-import basketIconBlack from "../../images/BasketIconBlackSvg.svg";
+import basketIconMob from "../../images/BasketIconMob.svg";
 
 function HeaderNav(props) {
   const { onCallPopup } = props;
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
   const handleResize = (event) => {
     setScreenWidth(event.target.innerWidth);
   };
-  
-      useEffect(() => {
-      window.addEventListener("resize", handleResize);
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }, []);
 
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <ul className="header-navigation">
-      <li> <Button src={phoneIcon} text={"Позвонить"} isHeader={"true"} onClick={onCallPopup} type="button" /></li>
-      <li> <Button src={favoritesIcon} text={"Избранное"} isHeader={"true"} /></li>
-      <li> <Button src={profileIcon} text={"Профиль"} isHeader={"true"}/></li>
-      <li> <Button src={screenWidth < 768 ? basketIconBlack : basketIconWhite} text={"Корзина"} isBasket={"true"} /></li>
+      <li className="header-navigation__link">
+        
+        <Button
+          src={screenWidth <= 768 ? phoneIconMob : phoneIcon}
+          text={"Позвонить"}
+          isHeader={"true"}
+          onClick={onCallPopup}
+          type="button"
+        />
+      </li>
+      <li className="header-navigation__link">
+        
+        <Button
+          src={screenWidth <= 768 ? favouritesIconMob : favouritesIcon}
+          text={"Избранное"}
+          isHeader={"true"}
+        />
+      </li>
+      <li className="header-navigation__link">
+        
+        <Button src={profileIcon} text={"Профиль"} isHeader={"true"} />
+      </li>
+      <li className="header-navigation__link">
+        
+        <Button
+          src={screenWidth <= 768 ? basketIconMob : basketIconWhite}
+          text={"Корзина"}
+          isBasket={"true"}
+        />
+      </li>
     </ul>
   );
 }
